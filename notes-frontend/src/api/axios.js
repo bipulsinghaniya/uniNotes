e44e1b3b488e19b57bd2a95fee7af6fc4a707aff
Bipul Singhaniya <singhaniyabipul9@gmail.com>
2026-01-11 16:36:50 +0530

    fron

--- notes-frontend/src/api/axios.js
+++ notes-frontend/src/api/axios.js
@@ -1,8 +1,12 @@
 import axios from "axios";
 
 const api = axios.create({
- baseURL: import.meta.env.VITE_API_BASE_URL,
-  withCredentials: true
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+  },
+  timeout: 10000,
 });
 
 export default api;
