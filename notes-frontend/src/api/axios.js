b8646bcc2b1f22a7613bf76b69c5f1aac76a7171
Bipul Singhaniya <singhaniyabipul9@gmail.com>
2026-01-11 16:51:34 +0530

    fix: correct register API path

--- notes-frontend/src/api/axios.js
+++ notes-frontend/src/api/axios.js
@@ -1,12 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: import.meta.env.VITE_API_BASE_URL, // https://uninotes-sm14.onrender.com
   withCredentials: true,
-  headers: {
-    "Content-Type": "application/json",
-  },
-  timeout: 10000,
 });
 
 export default api;
